c13608bf58bacc62a8dd7f1d9fa7603efff355ed
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-04-17 20:51:05 -0300

    Fix theme to use grey shades

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,8 @@
 const theme = {
     palette: {
         backgroundColor: '#FFFFFF',
-        primaryColor: '#00171F',
+        // primaryColor: '#00171F',
+        primaryColor: '#ddd',
         primaryLightColor: '#003459',
         secondaryColor: '#007EA7',
         secondaryLightColor: '#00A8E8'
