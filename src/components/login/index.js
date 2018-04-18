af3ddfe9d90370e6f13446db0c4c698f114a46b4
Afonso Delgado <afonsodelgadoss@gmail.com>
2018-04-17 21:37:49 -0300

    Add basic animated overlay to screen when Login button is pressed

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,13 +1,19 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { LayoutAnimation, Dimensions } from 'react-native';
 import { FullScreenContainer, IconTextField } from './../common';
 import { Button, Logo, TextFieldWrapper } from './components';
 import theme from './../../theme';
 
+const windowHeight = Dimensions.get('window').height;
+
 type Props = {};
 type State = {
     dataUsername: string,
-    dataPassword: string
+    dataPassword: string,
+    isLoggedIn: boolean,
+    overlayHeight: number,
+    overlayRadius: number
 };
 
 class Login extends Component<Props, State> {
@@ -15,7 +21,10 @@ class Login extends Component<Props, State> {
         super(props);
         this.state = {
             dataUsername: '',
-            dataPassword: ''
+            dataPassword: '',
+            isLoggedIn: false,
+            overlayHeight: 0,
+            overlayRadius: windowHeight / 2
         };
     }
 
@@ -31,43 +40,61 @@ class Login extends Component<Props, State> {
         this.setState(newState);
     };
 
+    handleLoginPress = () => {
+        setTimeout(() => {
+            LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+            this.setState({ overlayHeight: windowHeight, overlayRadius: 0 });
+        }, 2000);
+    };
+
     render() {
         return (
-            <FullScreenContainer justifyContent='space-around' behavior={'padding'}>
-                <Logo height={150} width={150} />
+            <Fragment>
+                <FullScreenContainer justifyContent='space-around' behavior={'padding'}>
+                    <Logo height={150} width={150} />
 
-                <TextFieldWrapper>
-                    <IconTextField
-                        id='dataUsername'
-                        iconName='md-person' iconColor={theme.palette.primaryColor}
-                        placeholder='Login'
-                        autoCorrect={false}
-                        autoCapitalize='none'
-                        selectionColor={theme.palette.primaryColor}
-                        marginBottom='16px'
-                        value={this.state.dataUsername}
-                        onChangeText={(value) => this.handleFieldChange(value, 'dataUsername')}
-                        onEraseField={this.handleFieldErase}
-                    />
+                    <TextFieldWrapper>
+                        <IconTextField
+                            id='dataUsername'
+                            iconName='md-person' iconColor={theme.palette.primaryColor}
+                            placeholder='Login'
+                            autoCorrect={false}
+                            autoCapitalize='none'
+                            selectionColor={theme.palette.primaryColor}
+                            marginBottom='16px'
+                            value={this.state.dataUsername}
+                            onChangeText={(value) => this.handleFieldChange(value, 'dataUsername')}
+                            onEraseField={this.handleFieldErase}
+                        />
 
-                    <IconTextField
-                        id='dataPassword'
-                        iconName='md-lock' iconColor={theme.palette.primaryColor}
-                        placeholder='Password'
-                        autoCorrect={false}
-                        autoCapitalize='none'
-                        selectionColor={theme.palette.primaryColor}
-                        secureTextEntry
-                        marginBottom='48px'
-                        value={this.state.dataPassword}
-                        onChangeText={(value) => this.handleFieldChange(value, 'dataPassword')}
-                        onEraseField={this.handleFieldErase}
-                    />
+                        <IconTextField
+                            id='dataPassword'
+                            iconName='md-lock' iconColor={theme.palette.primaryColor}
+                            placeholder='Password'
+                            autoCorrect={false}
+                            autoCapitalize='none'
+                            selectionColor={theme.palette.primaryColor}
+                            secureTextEntry
+                            marginBottom='48px'
+                            value={this.state.dataPassword}
+                            onChangeText={(value) => this.handleFieldChange(value, 'dataPassword')}
+                            onEraseField={this.handleFieldErase}
+                        />
 
-                    <Button/>
-                </TextFieldWrapper>
+                        <Button onPress={this.handleLoginPress} />
+                    </TextFieldWrapper>
 
-            </FullScreenContainer>
+                </FullScreenContainer>
+
+                <FullScreenContainer
+                    backgroundColor='#ddd'
+                    style={{
+                        position: 'absolute',
+                        bottom: 0,
+                        height: this.state.overlayHeight,
+                        borderRadius: this.state.overlayRadius
+                    }}/>
+            </Fragment>
         );
     }
 }
